9b1314598807aacaeefe547a4dc31a559d0155f2
charlesfrisbee <char5653@uni.sydney.edu.au>
2023-04-12 12:40:58 +0200

    init

---
+++ src/pages/api/trpc/[trpc].ts
@@ -0,0 +1,21 @@
+import { createNextApiHandler } from "@trpc/server/adapters/next";
+
+import { env } from "~/env.mjs";
+import { createTRPCContext } from "~/server/api/trpc";
+import { appRouter } from "~/server/api/root";
+import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
+import { type NextRequest } from "next/server";
+
+export const config = {
+  runtime: "edge",
+  region: "fra1",
+};
+
+export default async function handler(req: NextRequest) {
+  return fetchRequestHandler({
+    endpoint: "/api/trpc",
+    router: appRouter,
+    req,
+    createContext: createTRPCContext,
+  });
+}
